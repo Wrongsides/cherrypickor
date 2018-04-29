283ea97aa95b4d315b2d582c843a5e3db6ea9a5d
Wrongsides <johnbyrne.1984@gmail.com>
2018-04-29 14:20:58 +0100

    add step def place holders

--- src/test/java/wrongsides/cherrypickor/cucumber/CucumberRunner.java
+++ src/test/java/wrongsides/cherrypickor/cucumber/CucumberRunner.java
@@ -1,10 +1,11 @@
-package wrongsides.cherrypickor.cucumber;
+package wrongsides.cherrypickor;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"classpath:features"})
+@CucumberOptions(features = {"classpath:features"}, glue = {"wrongsides.cucumber"})
 public class CucumberRunner {
+
 }
--- src/test/java/wrongsides/cherrypickor/cucumber/StepDefinitions.java
+++ src/test/java/wrongsides/cherrypickor/cucumber/StepDefinitions.java
@@ -1,4 +1,21 @@
 package wrongsides.cherrypickor.cucumber;
 
-public class StepDefinitions {
+import cucumber.api.PendingException;
+import cucumber.api.java8.En;
+
+public class StepDefinitions implements En {
+
+    public StepDefinitions() {
+
+        When("^I post a list of asteroids$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+
+        Then("^the result should be ordered highest value first$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+
+    }
 }
