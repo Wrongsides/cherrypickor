24c864e77c3629b930e155e8d40936be8668fc70
Wrongsides <johnbyrne.1984@gmail.com>
2018-05-16 00:14:54 +0100

    extract common stuff to esiAdapter

---
+++ src/main/java/wrongsides/cherrypickor/adapter/EsiAdapter.java
@@ -0,0 +1,32 @@
+package wrongsides.cherrypickor.adapter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import wrongsides.cherrypickor.config.environment.Config;
+
+import java.util.Optional;
+
+@Component
+public class EsiAdapter {
+
+    private Config config;
+    private RestTemplate restTemplate;
+
+    public EsiAdapter(Config config, RestTemplate restTemplate) {
+        this.config = config;
+        this.restTemplate = restTemplate;
+    }
+
+    public <T extends isSearchResult> Optional<String> find(String name, String category, Class<T> clazz) {
+        String url = String.format("%s/%s/search/?datasource=%s&categories=%s&search=%s&strict=true",
+                config.getEsiUrl(), config.getEsiVersion(), config.getEsiDatasource(), category, name);
+
+        T object = restTemplate.getForObject(url, clazz);
+
+        if(object == null || object.getIds().isEmpty()) {
+            return Optional.empty();
+        } else {
+            return Optional.of(object.getIds().get(0));
+        }
+    }
+}
--- src/main/java/wrongsides/cherrypickor/domain/InventoryType.java
+++ src/main/java/wrongsides/cherrypickor/domain/InventoryType.java
@@ -2,22 +2,23 @@ package wrongsides.cherrypickor.domain;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import wrongsides.cherrypickor.adapter.isSearchResult;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class InventoryType {
+public class InventoryType implements isSearchResult {
 
     public InventoryType() { }
 
     @JsonAlias("inventory_type")
-    private List<String> inventoryTypeIds;
+    private List<String> ids;
 
-    public List<String> getInventoryTypeIds() {
-        if (inventoryTypeIds == null) {
-            inventoryTypeIds = new ArrayList<>();
+    public List<String> getIds() {
+        if (ids == null) {
+            ids = new ArrayList<>();
         }
-        return inventoryTypeIds;
+        return ids;
     }
 }
