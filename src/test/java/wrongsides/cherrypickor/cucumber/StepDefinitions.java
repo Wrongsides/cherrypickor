abf4e640d86f1cc06742fca63410817d90bdced8
Wrongsides <johnbyrne.1984@gmail.com>
2018-06-28 23:37:21 +0100

    add verifications

--- src/test/java/wrongsides/cherrypickor/cucumber/StepDefinitions.java
+++ src/test/java/wrongsides/cherrypickor/cucumber/StepDefinitions.java
@@ -44,8 +44,7 @@ public class StepDefinitions implements En {
         });
 
         And("^ESI services are available$", () -> {
-            stubForSpodBuyOrder();
-            stubForCrokBuyOrder();
+            stubForMarkets();
             stubForSearches();
             stubForTypes();
             stubForGroups();
@@ -79,16 +78,15 @@ public class StepDefinitions implements En {
         });
 
         Then("^the asteroid types are requested from ESI$", () -> {
-
+            verify(3, getRequestedFor(urlEqualTo("/latest/universe/types/1232")));
+            verify(2, getRequestedFor(urlEqualTo("/latest/universe/types/17436")));
         });
     }
 
-    private void stubForSpodBuyOrder() {
+    private void stubForMarkets() {
         stubFor(get("/latest/markets/10000002/orders/?datasource=tranquility&order_type=buy&type_id=17466")
                 .willReturn(okJson("[{\"duration\":90,\"is_buy_order\":true,\"issued\":\"2018-04-24T18:55:25Z\",\"location_id\":60004240,\"min_volume\":1,\"order_id\":5144382023,\"price\":2256.0,\"range\":\"region\",\"system_id\":30000128,\"type_id\":17466,\"volume_remain\":41838,\"volume_total\":42000},{\"duration\":90,\"is_buy_order\":true,\"issued\":\"2018-03-03T06:08:15Z\",\"location_id\":60003760,\"min_volume\":1,\"order_id\":5102339341,\"price\":0.01,\"range\":\"10\",\"system_id\":30000142,\"type_id\":17466,\"volume_remain\":1000000,\"volume_total\":1000000},{\"duration\":30,\"is_buy_order\":true,\"issued\":\"2018-04-17T15:45:38Z\",\"location_id\":60003760,\"min_volume\":1,\"order_id\":5129544024,\"price\":3050.68,\"range\":\"1\",\"system_id\":30000142,\"type_id\":17466,\"volume_remain\":24326,\"volume_total\":30000},{\"duration\":90,\"is_buy_order\":true,\"issued\":\"2018-05-01T21:55:18Z\",\"location_id\":1023164547009,\"min_volume\":1,\"order_id\":5049308512,\"price\":3050.7,\"range\":\"1\",\"system_id\":30000144,\"type_id\":17466,\"volume_remain\":70111,\"volume_total\":100000},{\"duration\":90,\"is_buy_order\":true,\"issued\":\"2018-05-02T06:02:18Z\",\"location_id\":1025949280066,\"min_volume\":100,\"order_id\":5131189070,\"price\":3050.71,\"range\":\"20\",\"system_id\":30000144,\"type_id\":17466,\"volume_remain\":88407,\"volume_total\":100000}]")));
-    }
 
-    private void stubForCrokBuyOrder() {
         stubFor(get("/latest/markets/10000002/orders/?datasource=tranquility&order_type=buy&type_id=17432")
                 .willReturn(okJson("[{\"duration\":90,\"is_buy_order\":true,\"issued\":\"2018-04-24T12:29:57Z\",\"location_id\":60004240,\"min_volume\":1,\"order_id\":5144139716,\"price\":1927.0,\"range\":\"region\",\"system_id\":30000128,\"type_id\":17432,\"volume_remain\":50000,\"volume_total\":50000},{\"duration\":30,\"is_buy_order\":true,\"issued\":\"2018-04-07T15:20:12Z\",\"location_id\":60003760,\"min_volume\":1,\"order_id\":5127497244,\"price\":2483.21,\"range\":\"1\",\"system_id\":30000142,\"type_id\":17432,\"volume_remain\":21805,\"volume_total\":35000},{\"duration\":90,\"is_buy_order\":true,\"issued\":\"2018-04-07T08:31:40Z\",\"location_id\":1023164547009,\"min_volume\":1,\"order_id\":4801695195,\"price\":2483.2,\"range\":\"1\",\"system_id\":30000144,\"type_id\":17432,\"volume_remain\":74192,\"volume_total\":100000},{\"duration\":90,\"is_buy_order\":true,\"issued\":\"2018-04-17T18:23:03Z\",\"location_id\":1023075604524,\"min_volume\":100,\"order_id\":5138782414,\"price\":2483.22,\"range\":\"20\",\"system_id\":30000144,\"type_id\":17432,\"volume_remain\":98324,\"volume_total\":100000}]")));
     }
