e1df17ed27004c33311daa44a5ac066cf1a3faa2
Wrongsides <johnbyrne.1984@gmail.com>
2018-06-29 18:11:29 +0100

    add docker file and use Spring Profiles for config

--- src/main/java/wrongsides/cherrypickor/config/environment/LocalConfig.java
+++ src/main/java/wrongsides/cherrypickor/config/environment/LocalConfig.java
@@ -1,5 +1,10 @@
 package wrongsides.cherrypickor.config.environment;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("local")
 public class LocalConfig extends Config {
 
     @Override
